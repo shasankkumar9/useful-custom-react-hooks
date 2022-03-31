8cab14c591a16b0b3ad063ad0d0dfee49557081d
shasankkumar9 <shasankkumar9@gmail.com>
2022-03-31 19:51:23 +0530

    useToggle notes

--- src/1-useToggle/useToggle.js
+++ src/1-useToggle/useToggle.js
@@ -1,13 +1,19 @@
-import { useState } from "react"
+import { useState } from "react";
+
+/* 
+  toggleValue() -> negates the current boolean value
+  toggleValue(true) -> set value to true
+  toggleValue(false) -> set value to false
+*/
 
 export default function useToggle(defaultValue) {
-  const [value, setValue] = useState(defaultValue)
+  const [value, setValue] = useState(defaultValue);
 
   function toggleValue(value) {
-    setValue(currentValue =>
-      typeof value === "boolean" ? value : !currentValue
-    )
+    setValue((currentValue) =>
+      typeof value === "boolean" ? value : !currentValue,
+    );
   }
 
-  return [value, toggleValue]
+  return [value, toggleValue];
 }
